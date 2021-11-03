2f24e662ac3f5cfb074668138bf4312ac62f7019
viktor.kendrick <viktor.kendrick@gmail.com>
2021-11-03 11:44:17 -0400

    commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+echo 'start of script';
+echo '<br>';
+
+$action = $_GET['action'];
+
+if($action !=''){
+    switch($action){
+        case 'create':
+            createHero($_GET['name'], $_GET['tagline']);
+            break;
+        case 'read':
+            
+            break;
+        case 'update':
+            updateHero($_GET['name'], $_GET['tagline']);
+            break;
+        case 'delete':
+            deleteHero($_GET['name']);
+            break;
+        default:
+        init();
+    }
+    viewAllHeroes();
+}
+
+function init(){
+    $_SESSION['heroes'] = [];
+}
+//Session to store information
+
+//store an array, push, pop, splice
+
+//Create Hero
+function createHero ($name, $tagline){
+echo '<pre>' . print_r([$name, $tagline], 1) . '</pre>';
+array_push($_SESSION['heroes'], [$name, $tagline]);
+}
+//Read Heroes
+function viewAllHeroes(){
+echo '<pre> view' . print_r($_SESSION['heroes'], 1) . '</pre>';
+}
+//Update Hero
+function updateHero($name, $tagline){
+
+}
+//Delete Hero
+function deleteHero($name){
+
+}
+echo '<br>';
+echo 'End of script';
+
+
+
+?>
